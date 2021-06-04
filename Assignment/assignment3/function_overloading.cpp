3af2181bb87b07843103e7f09115543f90b96c8f
skan00 <anvisking@gmail.com>
2021-06-04 14:05:56 +0545

    functon_overloading

---
+++ Assignment/assignment3/function_overloading.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+ 
+void add(int a,int b,int c){
+  cout<<"Sum is :"<<a+b+c<<endl;
+}
+ 
+void add(int a,int b){
+  cout<<"Sum is :"<<a+b<<endl;
+}
+ 
+void add(int a,int b,int c,int d){
+  cout<<"Sum is :"<<a+b+c+d<<endl;
+}
+ 
+int main(){
+  add(2,4);
+  add(3,5,6);
+  add(1,2,3,4);
+ 
+  return 0;
+}
